5ed37b5a1a018167e65d727218f21eccd31d4430
b1700161 <b1700161@helplive.edu.my>
2021-04-10 02:11:30 +0800

    code injections - mostly done - no bf with datalist, charset only

--- CodeInjectionGUIcode.py
+++ CodeInjectionGUIcode.py
@@ -3,6 +3,7 @@ from CodeInjectionGUI import CodeInGUI
 def main():
     pen = Pendrill("temp")
     gui = CodeInGUI(pen)
+    # gui.top.mainloop()
     # url = "http://natas15.natas.labs.overthewire.org"
     # password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
     # gui.urlEntry.insert(0, url)
