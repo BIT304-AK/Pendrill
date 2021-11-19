58aef11d8ca69a0a8bda80760eefe2ee5d94b88b
ashburger <b1700161@helplive.edu.my>
2021-11-20 00:30:26 +0800

    Clean up

--- CodeInjectionGUI.py
+++ CodeInjectionGUI.py
@@ -21,17 +21,9 @@ class CodeInGUI:
         """GUI design."""
         self.pen = pen
         self.top = top
-        # top = tkinter.Tk()
-        # top.geometry('1000x600')
 
-        # Notebook(tabs).
         notebook = ttk.Notebook(top)
         notebook.pack(pady=10, expand=True)
-        # self.f1 = ttk.Frame(notebook)
-        # self.f1.pack(fill='both', expand=True)
-        # self.f1.columnconfigure(0, weight=1)
-        # self.f1.columnconfigure(1, weight=1)
-        # notebook.add(self.f1, text='Page 1')
 
         f2 = ttk.Frame(notebook)
         f2.pack(fill='both', expand=True)
@@ -39,56 +31,13 @@ class CodeInGUI:
         self.sf = ScrolledFrame(notebook)
         self.sf.pack(fill='both', expand=True)
         notebook.add(self.sf, text='Make Attack')
-        # self.f3 = ttk.Frame(sf)
         self.f3 = self.sf.display_widget(ttk.Frame)
-        # self.f3.pack(fill='both', expand=True)
         self.f3.columnconfigure(0, weight=1)
         self.f3.columnconfigure(1, weight=1)
-        # notebook.add(self.f3, text='Make Attack')
-        # makeAttackScrollbar = Scrollbar(self.f3)
-        # makeAttackScrollbar.grid(row=0, column=10, ipady=100)
-        # self.f3.configure(yscrollcommand=makeAttackScrollbar.set)
-        # sf.bind_scroll_wheel(self.f3)
-        # sf.bind_arrow_keys(self.f3)
-        
         
         self.urlEntry = urlEntry
         # Code to add widgets will go here...
-        # Frame 1
-        # self.attackTable = ttk.Treeview(f2, columns=('Attack No.', 'URL',
-        #                                 'Code', 'Data'), show='headings')
-        # title = tkinter.Label(self.f1, text="Code Injection", font="bold")
-        # title.grid(row=0, column=0)
-        # labelFrame = ttk.LabelFrame(self.f1)
-        # labelFrame.grid(row=1, column=0)
-        # urlLabel = tkinter.Label(labelFrame, text="URL: ")
-        # urlLabel.grid(row=1, column=0)
-        # self.urlEntry = ttk.Entry(labelFrame, textvariable='url', background="Red")
-        # self.urlEntry.grid(row=1, column=1, padx=5, pady=5)
-
-        # dataLabel = tkinter.Label(labelFrame, text="Data: ")
-        # dataLabel.grid(row=2, column=0)
-        # self.dataEntry = ttk.Entry(labelFrame, textvariable='data')
-        # self.dataEntry.grid(row=2, column=1, padx=5, pady=5)
-
-        # authUsernameLabel = tkinter.Label(labelFrame, text="Username: ")
-        # authUsernameLabel.grid(row=3, column=0)
-        # self.authUsernameEntry = ttk.Entry(labelFrame, textvariable='authUsername')
-        # self.authUsernameEntry.grid(row=3, column=1, padx=5, pady=5)
-
-        # authPasswordLabel = tkinter.Label(labelFrame, text="Password: ")
-        # authPasswordLabel.grid(row=4, column=0)
-        # self.authPasswordEntry = ttk.Entry(labelFrame, textvariable='authPassword')
-        # self.authPasswordEntry.grid(row=4, column=1, padx=5, pady=5)
-
-        # containsLabel = tkinter.Label(labelFrame, text="Contains: ")
-        # containsLabel.grid(row=5, column=0)
-        # self.containsEntry = ttk.Entry(labelFrame, textvariable='contains')
-        # self.containsEntry.grid(row=5, column=1, padx=5, pady=5)
-
-        # self.submitBtn = tkinter.Button(labelFrame, text="Submit",
-        #                                 command=lambda: self.submitReq())
-        # self.submitBtn.grid(row=6, column=3)
+        
         # Table
         self.attackTable = ttk.Treeview(f2, columns=('Attack No.', 'URL',
                                         'Code', 'Data','Cookies', 'Query', 'Contains'),
@@ -131,11 +80,7 @@ class CodeInGUI:
         title.grid(row=0, column=0)
         labelFrame = ttk.LabelFrame(self.f3)
         labelFrame.grid(row=1, column=0)
-        # urlLabel = tkinter.Label(labelFrame, text="URL: ")
-        # urlLabel.grid(row=1, column=0)
-        # self.urlEntry = ttk.Entry(labelFrame, textvariable='url')
-        # self.urlEntry.grid(row=1, column=1, padx=5, pady=5)
-
+        
         containsLabel = tkinter.Label(labelFrame, text="Contains: ")
         containsLabel.grid(row=0, column=0)
         self.bfContainsEntry = ttk.Entry(labelFrame, textvariable='contains')
@@ -172,23 +117,13 @@ class CodeInGUI:
         # Cookie section
         self.cookieSection = ttk.LabelFrame(self.f3, text="Cookies", name="cookieLabelFrame")
         self.cookieSection.grid(row=1, column=1)
-        # Cookie Table
-        # self.cookieTable = ttk.Treeview(self.cookieSection, columns=('Name', 'Value'),
-        #                                 show='headings')
-        # self.cookieTable.grid(column=0,row=0)
-        # self.cookieTable.heading('Name', text='Name')
-        # self.cookieTable.heading('Value', text='Value')
-        # self.cookieTable.grid(column=0,row=0)
+        
         # Add Cookie
         cookieNameLabel = tkinter.Label(self.cookieSection, text="Name: ")
         cookieNameLabel.grid(row=1, column=0)
         self.cookieNameEntry = ttk.Entry(self.cookieSection, textvariable='cookieName')
         self.cookieNameEntry.grid(row=1, column=1, padx=5, pady=5)
 
-        # cookieValueLabel = tkinter.Label(labelFrame3, text="Value: ")
-        # cookieValueLabel.grid(row=2, column=0)
-        # self.cookieValueEntry = ttk.Entry(labelFrame3, textvariable='cookieValue')
-        # self.cookieValueEntry.grid(row=2, column=1, padx=5, pady=5)
         self.cookieEntryRow = 5
         self.cookieIntvars = {}
         self.formIntvars = {}
@@ -197,17 +132,7 @@ class CodeInGUI:
                                           command=lambda: self.addCookie())
         self.cookieAddBtn.grid(row=3, column=0)
         self.bindScroll(self.sf, self.f3)
-        # url = "http://natas14.natas.labs.overthewire.org"
-        # password = "Lg96M10TdfaPyVBkJdjymbllQ5L6qdl1"
-        url = "http://natas18.natas.labs.overthewire.org"
-        password = "xvKIqDjy4OPv7wCRgDlmj0pFsCsDjhdP"
-        # url = "http://natas15.natas.labs.overthewire.org"
-        # password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
-        self.urlEntry.insert(0, url)
-        self.bfAuthUsernameEntry.insert(0, "natas18")
-        self.bfAuthPasswordEntry.insert(0, password)
 
-        # top.mainloop()
     def bindScroll(self, sf, parent):
         for child in parent.winfo_children():
             self.bindScroll(sf, child)
@@ -216,28 +141,6 @@ class CodeInGUI:
         sf.bind_scroll_wheel(parent)
         sf.bind_arrow_keys(parent)
     
-    def submitReq(self):
-        """Submit request."""
-        allowRedirects = bool(self.allowRedirectVar.get())
-        print("red", allowRedirects)
-        attack = self.pen.singleAtk(self.urlEntry.get(),
-                                    data=self.dataEntry.get(),
-                                    username=self.authUsernameEntry.get(),
-                                    password=self.authPasswordEntry.get(),
-                                    allow_redirects=allowRedirects)
-        if attack.response == '404':
-            messagebox.showerror("404 Error", "Url not resolved")
-        else:
-            if attack.response.status_code == 401:
-                messagebox.showinfo("401 Error", "Authentication headers required!")
-            if attack.response.status_code == 405:
-                messagebox.showinfo("405 Error", "This method not allowed!")
-            query = self.containsEntry.get()
-            contains = attack.responseContains(query)
-            self.addToTable(attack, query, contains, self.attackTable)
-            if attack.response.status_code != 401:
-                self.getFormInputs(attack)
-
     def submitReqBF(self):
         """Submit request."""
         allowRedirects = bool(self.allowRedirectVar.get())
@@ -255,7 +158,6 @@ class CodeInGUI:
         query = self.bfContainsEntry.get()
         contains = attack.responseContains(query)
         
-        # print("SubmitReqBF")
         self.addToTable(attack, query, contains, self.attackTable)
         if attack.response == '404':
             messagebox.showerror("404 Error", "Url not resolved")
@@ -276,10 +178,8 @@ class CodeInGUI:
             code = attack.response
         data = attack.data
         cookies = attack.cookies
-        # print("addToTable before insert")
         attackTable.insert("", index="end", values=(str(num), url, code, data, cookies,
                            query, str(contains)))
-        # print("addToTable after insert")
 
     def callback(self, sv):
         """Return StringVar."""
@@ -298,56 +198,19 @@ class CodeInGUI:
             i = i + len(k + "=" + v + "&")
             n = n + 1
 
-    def getFormInputs(self, attack):
-        """Return textbox value."""
-        for pos in self.f1.grid_slaves():
-            print('pos')
-            print(pos, pos.grid_info()['row'], pos.grid_info()['column'])
-            if int(pos.grid_info()['row']) == 5:
-                for child in pos.grid_slaves():
-                    child.destroy()
-                pos.destroy()
-        inputs = attack.retrieveInputs()
-        labelFrame = ttk.LabelFrame(self.f1, text='forms')
-        labelFrame.grid(row=5, column=0)
-        n = 2
-        stringVarList = []
-        self.inputList = {}
-        for i in inputs:
-            inputLabel = tkinter.Label(labelFrame, text=i['name'])
-            inputLabel.grid(row=n, column=0)
-            sv = StringVar(labelFrame, name=i['name'])
-            stringVarList.append(sv)
-            sv.trace("w", lambda index, mode, name=i['name'], sv=sv, x=i: self.dataEntrys(x['name'],
-            sv.get()))
-            inputEntry = ttk.Entry(labelFrame, textvariable=sv)
-            self.inputList[i['name']] = ""
-            inputEntry.grid(row=n, column=1, padx=5, pady=5)
-            n = n + 1
-        self.bindScroll(self.sf, labelFrame)
-
     def getFormInputsBF(self, attack):
         """Return textbox value."""
         for pos in self.f3.grid_slaves():
             print('pos')
             print(pos, pos.grid_info()['row'], pos.grid_info()['column'])
             if int(pos.grid_info()['row']) == 6:
-                # print(len(pos.winfo_children()))
                 for child in pos.grid_slaves():
-                    # pos.forget()
                     child.destroy()
                 pos.destroy()
         inputs = attack.retrieveInputs()
         self.currentAttack = attack
-        # inputNames = []
-        # for i in inputs:
-        #     inputNames.append(i['name'])
         labelFrame = ttk.LabelFrame(self.f3, text='forms', name='bfLabelFrame')
         labelFrame.grid(row=6, column=0)
-        # dropdown = StringVar()
-        # dropdown.set(inputNames[0])
-        # dropdownList = OptionMenu(labelFrame, dropdown, *inputNames)
-        # dropdownList.grid(row=1, column=0)
         c = 0
         self.formIntvars = {}
         self.bfTable = ttk.Treeview(labelFrame, show='headings')
@@ -359,7 +222,6 @@ class CodeInGUI:
             n = n + 7
             c = c + 2
             self.addColumn(i['name'])
-        # labelFrame.grid_rowconfigure(c, weight=8)
         if(len(inputs) > 0):
             self.saveDictChkState = IntVar(labelFrame)
             self.saveDictCheckbox = ttk.Checkbutton(labelFrame,
@@ -367,9 +229,6 @@ class CodeInGUI:
                                                     variable=self.saveDictChkState)
             self.saveDictCheckbox.grid(row=n+1, column=1, padx=5, pady=2)
             self.bfTable.grid(row=1, column=c, rowspan=8)
-        # self.bfBtn = tkinter.Button(labelFrame, text="Attack",
-        #                             command=lambda: self.bruteForceData())
-        # self.bfBtn.grid(row=n+2, column=1)
         self.bindScroll(self.sf, labelFrame)
 
     def bruteForceData(self):
@@ -384,13 +243,13 @@ class CodeInGUI:
         url = self.urlEntry.get()
         username = self.bfAuthUsernameEntry.get()
         password = self.bfAuthPasswordEntry.get()
-        allowRedirects = self.allowRedirectVar
-        
+        allowRedirects = bool(self.allowRedirectVar.get())
         cookieData = self.getInputData(self.cookieSection, self.cookies, cookie=True)
         
         self.bruteForce(url, inputData, datatype='charset',
                         username=username, password=password, allow_redirects=allowRedirects, cookieData=cookieData)
 
+    # Gets the entered data of list of inputs in a section
     def getInputData(self, location, inputs, cookie=False):
         inputData = []
         for i in inputs:
@@ -421,6 +280,7 @@ class CodeInGUI:
             inputData.append(x)
         return inputData
 
+    # launches a bruteforce attack
     def bruteForce(self, url, inputData, datatype=None,
                    contains=None, action=None, username=None, password=None, allow_redirects=True, cookieData=None):
         """Brute Force attack."""
@@ -458,7 +318,7 @@ class CodeInGUI:
             # the attacks
             payload = {}
             cookies = {}
-            for i in list(itertools.product(*formListList)): #itertools creates all the possible combinations for the payload
+            for i in list(itertools.product(*formListList)): #list contains all possible combinations for the payload
                 tempDict = []
                 for x in inputData: #adds each data entry to the payload
                     payload[x['name']] = x['prefix'] + i[inputData.index(x)] + x['suffix']
@@ -466,9 +326,6 @@ class CodeInGUI:
                 for c in list(itertools.product(*cookieListList)):
                     for cookie in cookieData:
                         cookies[cookie['name']] = cookie['prefix'] + c[cookieData.index(cookie)] + cookie['suffix']
-                    # attack = self.pen.createAttack(url, payload)
-                    # attack.postReq(data=payload, username=username,
-                    #            password=password, cookies=cookies, allow_redirects=allow_redirects)
                     attack = self.pen.singleAtk(url, data=payload, username=username,
                                password=password, cookies=cookies, allow_redirects=allow_redirects)
                     query = self.bfContainsEntry.get()
@@ -479,15 +336,13 @@ class CodeInGUI:
                                 self.addToBfTable(tempDict)
                     except AttributeError:
                         pass
-                    # self.pen.saveAttack(attack)
                     self.addToTable(attack, query, contains, self.attackTable)
                     self.top.update()
                     if contains is True:
-                        # natas16" and password LIKE BINARY "%
-                        print("test break", self.stopBfVar.get())
                         if self.stopBfVar.get() == 1:
-                            print("test break", self.stopBfVar.get())
                             return
+    
+    # checks to see if an entry set exists 
     def checkEntryExists(self, location, entryName):
         try: 
             location.nametowidget('data'+entryName)
@@ -495,6 +350,7 @@ class CodeInGUI:
             return False
         return True
     
+    # adds an entry set
     def addEntrySet(self, location, entryName, startRow, startCol, cookie=False):
         if self.checkEntryExists(location, entryName) == True:
             return
@@ -504,16 +360,12 @@ class CodeInGUI:
         prefixLabel = tkinter.Label(location, text="prefix")
         prefixLabel.grid(row=startRow+1, column=startCol)
 
-        # sv = StringVar()
-        # sv.trace("w", lambda name, index, mode)
         self.prefixEntry = ttk.Entry(location, name='prefix'+entryName)
         self.prefixEntry.grid(row=startRow+1, column=startCol+1, padx=5, pady=5)
 
         suffixLabel = tkinter.Label(location, text="suffix")
         suffixLabel.grid(row=startRow+2, column=startCol)
-        # sv = StringVar()
-        # sv.trace("w", lambda name, index, mode, sv=sv: self.dataEntrys(entryName,
-        # inputEntry.get()))
+        
         self.suffixEntry = ttk.Entry(location, name='suffix'+entryName)
         self.suffixEntry.grid(row=startRow+2, column=startCol+1, padx=5, pady=5)
 
@@ -525,16 +377,14 @@ class CodeInGUI:
         inputLabel = tkinter.Label(location, text='Min Data Len')
         inputLabel.grid(row=startRow+4, column=startCol)
         self.minEntry = ttk.Entry(location, name='min'+entryName)
-        # self.minEntry.place(width=2)
         self.minEntry.grid(row=startRow+4, column=startCol+1, padx=5, pady=2)
 
         inputLabel = tkinter.Label(location, text='Max Data Len')
         inputLabel.grid(row=startRow+5, column=startCol)
         self.maxEntry = ttk.Entry(location, name='max'+entryName)
-        # self.maxEntry.place(width=2)
         self.maxEntry.grid(row=startRow+5, column=startCol+1, padx=5, pady=2)
-        self.saveBfChkState = IntVar(location)
 
+        self.saveBfChkState = IntVar(location)
         self.saveBfCheckbox = ttk.Checkbutton(location,
                                                 text='Bruteforce',
                                                 variable=self.saveBfChkState, name='tbf'+entryName)
@@ -543,6 +393,7 @@ class CodeInGUI:
             deleteCookieBtn = tkinter.Button(location, text="Delete Cookie",name="del"+entryName, command=lambda: self.deleteCookie(entryName))
             deleteCookieBtn.grid(row=startRow+7, column=startCol+2)
     
+    # deletes a cookie entry set
     def deleteCookie(self, name):
         bfChk = self.cookieSection.nametowidget('tbf' + name)
         endRow = int(bfChk.grid_info()['row'])
@@ -553,6 +404,7 @@ class CodeInGUI:
         self.cookieIntvars.pop('tbf'+name, None)
         self.cookies.remove({'name':name})
     
+    # adds a column to the brute force table
     def addColumn(self, newCol):
         """Add a col."""
         currentCols = list(self.bfTable['columns'])
@@ -568,7 +420,7 @@ class CodeInGUI:
             # state = currentCols[i].pop('state')
             self.bfTable.heading(key, text=key)
             i = i + 1
-
+    
     def addToBfTable(self, data):
         """Add to Brute Force Table."""
         tempList = []
@@ -577,6 +429,7 @@ class CodeInGUI:
         val = tuple(tempList)
         self.bfTable.insert("", index="end", values=val)
 
+    # Adds a cookie entry set
     def addCookie(self):
         name = self.cookieNameEntry.get()
         # value = self.cookieValueEntry.get()
@@ -591,7 +444,7 @@ class CodeInGUI:
         self.cookieNameEntry.delete(0,END)
         # self.cookieValueEntry.delete(0,END)
         self.bindScroll(self.sf, self.cookieSection)
-
+    
     def showResponse(self, event):
         """Shows Selected Response"""
         index = self.attackTable.item(self.attackTable.selection())['values'][0]
